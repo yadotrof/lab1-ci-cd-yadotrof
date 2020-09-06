3f66aea2b57e268919ce72297ed85a5051982fa3
Gleb Shmarov <gshmarov@parallels.com>
2020-09-06 23:47:48 +0300

    Add swagger and openapi schema

--- lab1/urls.py
+++ lab1/urls.py
@@ -3,6 +3,8 @@ lab1 URL Configuration.
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 from lab1.person.views import PersonViewSet
 
 person_all = PersonViewSet.as_view({
@@ -17,6 +19,14 @@ person_concrete = PersonViewSet.as_view({
 })
 
 urlpatterns = [
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='person/swagger.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+    path('openapi', get_schema_view(title="RSOI lab1",
+                                    description="API for person",
+                                    version="1.0.0"
+                                    ), name='openapi-schema'),
     path('persons/', person_all, name='person-all'),
     path('persons/<int:pk>/', person_concrete, name='person-concrete'),
     path('admin/', admin.site.urls),
